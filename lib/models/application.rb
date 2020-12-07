8856b3c88ce5f417cafcf33a37f169b34bb7a4a8
Jason Zhou <jzhou35@buffalo.edu>
2020-12-07 14:18:44 -0500

    Done with Project

--- lib/models/application.rb
+++ lib/models/application.rb
@@ -4,25 +4,20 @@ class Application
     attr_accessor :user
 
     @@prompt = TTY::Prompt.new
-    # def initialize
-    #     @prompt = TTY::Prompt.new
-    # end
 
-    def welcome                             
-        shef = """             ,. -,          .·¨'`;        ,.·´¨;\                  _,.,  °             ,.-·~ ^~,   
-        ,.·'´,    ,'\        ';   ;'\       ';   ;::\          ,.·'´  ,. ,  `;\ '        .'´  ,.-·~-.,':\  
-    ,·'´ .·´'´-·'´::::\'      ;   ;::'\      ,'   ;::';       .´   ;´:::::\`'´ \'\       ;   ,':\::::::'\::\ 
-   ;    ';:::\::\::;:'       ;  ;::_';,. ,.'   ;:::';°     /   ,'::\::::::\:::\:'     ;'   ;:::'_-·~-\;' 
-   \·.    `·;:'-·'´        .'     ,. -·~-·,   ;:::'; '    ;   ;:;:-·'~^ª*';\'´     ,.'    'ª*'´ __'`;\ ° 
-    \:`·.   '`·,  '        ';   ;'\::::::::;  '/::::;      ;  ,.-·:*'´¨'`*´\::\ '    `',   ;\¯::::::::\:\ 
-      `·:'`·,   \'          ;  ';:;\;::-··;  ;::::;      ;   ;\::::::::::::'\;'      ;   ;::\;::_:_::\;' 
-       ,.'-:;'  ,·\         ':,.·´\;'    ;' ,' :::/  '     ;  ;'_\_:;:: -·^*';\      ;  ';:::;        '    
-  ,·'´     ,.·´:::'\         \:::::\    \·.'::::;        ';    ,  ,. -·:*'´:\:'\°    ';  ;::';             
-   \`*'´\::::::::;·'          \;:·´     \:\::';          \`*´ ¯\:::::::::::\;' '    \*´\:';        '    
-    \::::\:;:·´                          `·\;'             \:::::\;::-·^*'´          '\::\;     '        
-      '`*'´                                 '                `*´¯                     `*´              """
-
-      shef2 = """
+    def welcome                 
+        @main_menu_display = """
+    ███▄ ▄███▓ ▄▄▄       ██▓ ███▄    █     ███▄ ▄███▓▓█████  ███▄    █  █    ██    
+    ▓██▒▀█▀ ██▒▒████▄    ▓██▒ ██ ▀█   █    ▓██▒▀█▀ ██▒▓█   ▀  ██ ▀█   █  ██  ▓██▒   
+    ▓██    ▓██░▒██  ▀█▄  ▒██▒▓██  ▀█ ██▒   ▓██    ▓██░▒███   ▓██  ▀█ ██▒▓██  ▒██░   
+    ▒██    ▒██ ░██▄▄▄▄██ ░██░▓██▒  ▐▌██▒   ▒██    ▒██ ▒▓█  ▄ ▓██▒  ▐▌██▒▓▓█  ░██░   
+    ▒██▒   ░██▒ ▓█   ▓██▒░██░▒██░   ▓██░   ▒██▒   ░██▒░▒████▒▒██░   ▓██░▒▒█████▓    
+    ░ ▒░   ░  ░ ▒▒   ▓▒█░░▓  ░ ▒░   ▒ ▒    ░ ▒░   ░  ░░░ ▒░ ░░ ▒░   ▒ ▒ ░▒▓▒ ▒ ▒    
+    ░  ░      ░  ▒   ▒▒ ░ ▒ ░░ ░░   ░ ▒░   ░  ░      ░ ░ ░  ░░ ░░   ░ ▒░░░▒░ ░ ░    
+    ░      ░     ░   ▒    ▒ ░   ░   ░ ░    ░      ░      ░      ░   ░ ░  ░░░ ░ ░    
+           ░         ░  ░ ░           ░           ░      ░  ░         ░    ░    
+    """            
+      shef_logo = """
                                                                                                
                    ___                  .-.                    ./#&@@@@@@@@%’*  
                   (   )                /   `.         ,@@@@@@/‘            @@
@@ -43,20 +38,13 @@ class Application
                     ╚═╝╩  ╩╚═╝╚═╝╚═╝  ╩ ╩╚═╝╩╚═╚═╝╚═╝             
              ┬   ╔═╗╦  ╦╔═╗╦═╗╦ ╦╔╦╗╦ ╦╦╔╗╔╔═╗  ╔═╗╦╔╗╔╔═╗
             ┌┼─  ║╣ ╚╗╔╝║╣ ╠╦╝╚╦╝ ║ ╠═╣║║║║║ ╦  ╠╣ ║║║║║╣ 
-            └┘   ╚═╝ ╚╝ ╚═╝╩╚═ ╩  ╩ ╩ ╩╩╝╚╝╚═╝  ╚  ╩╝╚╝╚═╝ 
-                                                                                                                                                                                                                                                                                                                                                                                                                          
+            └┘   ╚═╝ ╚╝ ╚═╝╩╚═ ╩  ╩ ╩ ╩╩╝╚╝╚═╝  ╚  ╩╝╚╝╚═╝                                                                                                                                                                                                                                                                                                                                                                                                                    
       """
-        puts shef2
+        puts shef_logo.green
     end
-
-    def login_or_register
-        @@prompt.select("Would you like to register or login?") do |menu|
-            menu.choice "Register", -> {register}
-            menu.choice "Login", -> {login}
-        end
-    end
-
     
+    #LOGIN AND REGISTER HELPER METHODS#
+
     def register
         User.register_a_user
     end
@@ -65,39 +53,44 @@ class Application
         User.login_a_user
     end
 
+    def login_or_register
+        @@prompt.select("Would you like to register or login?".blue, symbols: { marker: "🍑"}) do |menu|
+            menu.choice "Register", -> {register}
+            menu.choice "Login", -> {login}
+        end
+    end
+    
+    #CART METHODS AND HELPER METHODS# 
+    
     def checkout_helper
         user.check_out_cart
         main_menu
     end
-
+    
     def cart_helper
-        user.display_cart
-        @@prompt.select("Would you like to checkout?") do |menu|
-            menu.choice "Check out", -> {checkout_helper}
-            menu.choice "Go back to main menu", -> {main_menu}
+        if user.current_cart.product_orders.count == 0
+            puts "You don't have any products in your cart currently😡"
+            main_menu
+        else
+            user.display_cart
+            @@prompt.select("Would you like to checkout?".blue, symbols: { marker: "💰"}) do |menu|
+                menu.choice "Check out", -> {checkout_helper}
+                menu.choice "Go back to main menu", -> {main_menu}
+            end
         end
     end
-
+    
     def past_orders_helper
         past_orders = user.past_orders
-        if past_orders.nil?
-           puts "There are no past orders. Would you like to hear about our loyalty rewards program?"
-           main_menu
+        if past_orders.count == 0
+            puts "There are no past orders. Would you like to hear about our loyalty rewards program?🤑"
+            main_menu
         end 
         choices = Hash[past_orders.map {|order| [order.updated_at, order]}]
-        @past_order = @@prompt.select("Which past order would you like to view?", choices)
+        @past_order = @@prompt.select("Which past order would you like to view?".blue, choices, symbols: { marker: "📁"})
         choices = Hash[past_orders.map {|order| [order.updated_at, past_cart]}]
     end
-
-    def past_cart
-        names = @past_order.product_orders.map {|product_order| "#{product_order.product.name} with Quantity of #{product_order.quantity}"}
-        puts "#{names}"
-        @@prompt.select("What would you like to do?") do |menu| 
-            menu.choice "Order again", -> {order_again_helper}
-            menu.choice "Go back to main menu", -> {main_menu}
-        end
-    end
-
+    
     def order_again_helper
         new_order_product = @past_order.product_orders.map {|product_order| product_order}
         new_order_product.each do |product_order|
@@ -105,27 +98,49 @@ class Application
         end 
         main_menu
     end
-
+    
+    def past_cart
+        names = @past_order.product_orders.map {|product_order| "#{product_order.product.name} with Quantity of #{product_order.quantity}"}
+        puts "#{names}"
+        @@prompt.select("What would you like to do?".blue, symbols: { marker: "📂"}) do |menu| 
+            menu.choice "Order again", -> {order_again_helper}
+            menu.choice "Go back to main menu", -> {main_menu}
+        end
+    end
+    
+    #PRODUCT METHODS AND HELPER METHODS#
+    
     def view_all_products
         arr_of_products = User.all_product_instances #Array of Product INSTANCES
         choices = Hash[arr_of_products.map {|product| [product.name, product]}] #HASH: product = NAME OF PRODUCT, VALUE = PRODUCT INSTANCE           
-        $selection = @@prompt.select("Which product would you like to view?", choices) #$ means global var. 
+        $selection = @@prompt.select("Which product would you like to view?".blue, choices, symbols: { marker: "‍🍽"}) #$ means global var. 
         choices = Hash[arr_of_products.map {|product| [product, item_options]}]
     end 
-
+    
     def view_all_category_products
         arr_of_cat_products = User.all_product_instances.select {|product| product.pairing == $category}.map {|product| product}
         choices = Hash[arr_of_cat_products.map {|product| [product.name, product]}]
-        $selection = @@prompt.select("Which product would you like to view?", choices)
+        $selection = @@prompt.select("Which product would you like to view?".blue, choices, symbols: { marker: "🔪"})
         choices = Hash[arr_of_cat_products.map {|product| [product.name, item_options]}]
     end
     
     def view_by_pairings 
         arr_of_pairings = User.categories 
         choices = Hash[arr_of_pairings.map{ |pairing| [pairing.name, pairing]}]
-        $category = @@prompt.select("Which pairing would you like to view?", choices)
+        $category = @@prompt.select("Which pairing would you like to view?".blue, choices, symbols: { marker: "👯‍♂️"})
         view_all_category_products 
     end 
+
+    def item_menu
+        #system("clear")
+        @@prompt.select("Would you like to view all products or by pairing?".blue, symbols: { marker: "✅"}) do |menu|
+            menu.choice "View ALL Products", -> {view_all_products}
+            menu.choice "View by pairs", -> {view_by_pairings}
+            menu.choice "Go Back", -> {main_menu}
+        end
+    end 
+
+    #REVIEW METHODS AND HELPERS#
 
     def add_review
         user.add_review
@@ -139,20 +154,20 @@ class Application
 
     def my_reviews
         arr_of_products = user.my_reviewed_products
-        if arr_of_products.nil?
-            puts "You haven't written a review."
+        if arr_of_products.count == 0
+            puts "You haven't written a review😩"
             main_menu
         end
         choices = Hash[arr_of_products.map {|product| [product.name, product]}]
-        @review_choice = @@prompt.select("Which product's review would you like to look at?", choices)
+        @review_choice = @@prompt.select("Which product's review would you like to look at?".blue, choices, symbols: { marker: "👀"})
         show_reviews
     end
 
     def show_reviews 
-        puts @review_choice.reviews.map {|review| "Review id #{review.id}: #{review.description} with a rating of #{review.rating}/5!" if review.user == user}.compact
-        @@prompt.select("Would you like to delete any reviews or go back?") do |menu|
+        puts @review_choice.reviews.map {|review| "Review id #{review.id}: #{review.description} with a rating of #{review.rating}/5!".yellow if review.user == user}.compact
+        @@prompt.select("Would you like to delete any reviews or go back?".blue, symbols: { marker: "💣"}) do |menu|
             menu.choice "Delete a review", -> {delete_review}
-            menu.choice "Go Back to Main Menu", -> {main_menu}
+            menu.choice "Go Back to main menu", -> {main_menu}
         end
     end 
 
@@ -167,10 +182,14 @@ class Application
         main_menu
     end
 
+
+    #MANAGING THE CART 
+
     def item_options
-        @@prompt.select("What would you like to do?") do |menu|
-            menu.choice "Add a review", -> {add_review}
+        puts $selection.description
+        @@prompt.select("What would you like to do?".blue, symbols: { marker: "✅"}) do |menu|
             menu.choice "Add to cart", -> {add_to_cart_helper} 
+            menu.choice "Add a review", -> {add_review}
             menu.choice "View all reviews", -> {reviews_for_product}
             menu.choice "Go back to main menu", -> {main_menu}
         end
@@ -179,12 +198,15 @@ class Application
     def add_to_cart_helper 
         puts "Quantity?"
         quantity = gets.chomp
+        while quantity == '0'
+            puts "Please enter a valid quantity!😡"
+            quantity = gets.chomp
+        end
         user.add_to_cart($selection, quantity)
         main_menu
     end
 
     def delete_from_cart_helper
-        
         puts "Which Product Order would you like to delete? Please input the Product Order ID number."
         product_order_id = gets.chomp 
         user.delete_product_order_by_id(product_order_id)
@@ -192,7 +214,7 @@ class Application
     end
 
     def update_quantity_helper 
-        puts "Which Product Order would you like to update? Please input the ID number"
+        puts "Which Product Order would you like to update? Please input the ID number."
         product_order_id = gets.chomp
         puts "What is the new quantity of the product you would like?"
         new_quantity = gets.chomp 
@@ -206,38 +228,39 @@ class Application
     end
 
     def update_cart_helper
-        @@prompt.select("Would you like to delete an item entirely or update cart?") do |menu|
-            menu.choice "Delete a product", -> {delete_from_cart_helper}
-            menu.choice "Update cart", -> {update_quantity_helper}
-            menu.choice "Empty cart", -> {empty_cart}
-            menu.choice "Go to main menu",  -> {main_menu}
+        if user.current_cart.product_orders.count == 0 
+            puts "Sorry you don't have any items in your cart right now. Please go check out our products🥺"
+            main_menu
+        else 
+            @@prompt.select("Would you like to delete an item entirely or update cart?".blue, symbols: { marker: "🧨"}) do |menu|
+                menu.choice "Delete a product", -> {delete_from_cart_helper}
+                menu.choice "Update cart", -> {update_quantity_helper}
+                menu.choice "Empty cart", -> {empty_cart}
+                menu.choice "Go to main menu",  -> {main_menu}
+            end
         end
     end
     
+
+    #MAIN MENU
+
     def main_menu
+        sleep 2
+        puts @main_menu_display.red
         user.reload
-        @@prompt.select("Welcome, #{user.username}! 🥳 What would you like to do?".blue.on_white.bold) do |menu|
-            menu.choice "View Cart".red, -> {cart_helper} 
-            menu.choice "Update Cart".red, -> {update_cart_helper}
-            menu.choice "View Past Orders".red, -> {past_orders_helper}
-            menu.choice "View My Reviews".red, -> {my_reviews}
-            menu.choice "View Products".red, -> {item_menu} 
-            menu.choice "Log Out".red, -> {log_out}
+        @@prompt.select("Welcome, #{user.username}! 🥳 What would you like to do?".blue, symbols: { marker: "🍁"}) do |menu|
+            menu.choice "View Products", -> {item_menu} 
+            menu.choice "View Cart", -> {cart_helper} 
+            menu.choice "Update Cart", -> {update_cart_helper}
+            menu.choice "View Past Orders", -> {past_orders_helper}
+            menu.choice "View My Reviews", -> {my_reviews}
+            menu.choice "Log Out", -> {log_out}
         end
     end
 
-    def item_menu
-        @@prompt.select("Would you like to view all products or by pairing?") do |menu|
-            menu.choice "View ALL Products", -> {view_all_products}
-            menu.choice "View by pairs", -> {view_by_pairings}
-            menu.choice "Go Back", -> {main_menu}
-        end
-    end 
-
     def log_out
-        puts "Thanks for checking out our app! Please like comment and subscribe"
+        puts "Thanks for checking out our app! Please like, comment, and subscribe 🥺"
         exit 
     end
 
 end 
-
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,3 +1,4 @@
+require 'progress_bar'
 class User < ActiveRecord::Base
 
     has_many :reviews
@@ -8,35 +9,62 @@ class User < ActiveRecord::Base
     @@prompt = TTY::Prompt.new
     @@sym = @@prompt.decorate('🤫')
 
+    #CREATING PROGRESS BAR# 
+
+    def self.progress_bar
+        bar = ProgressBar.new 
+        100.times do 
+            sleep 0.02
+            bar.increment!
+        end
+    end
+
+    #LOGIN AND REGISTER METHODS# 
+
     def self.login_a_user
-        puts "Please enter your username"
+        puts "Please enter your username:".green
         username = gets.chomp
-        password = @@prompt.mask("Please enter your password:", mask: @@sym)
+        password = @@prompt.mask("Please enter your password:".green, mask: @@sym)
         user = User.find_by(username: username, password: password)
         if user.nil?
-            puts "Sorry there's no one with that username or password"
+            puts "Sorry there's no one with that username or password.".green
         else 
+            progress_bar
             user 
         end
     end
 
     def self.register_a_user
-        username = @@prompt.ask("Please enter a username")
-        password = @@prompt.mask("Please enter a password", mask: @@sym)
-        name = @@prompt.ask("Please enter your name")
+        username = @@prompt.ask("Please enter a username:".green)
+        password = @@prompt.mask("Please enter a password:".green, mask: @@sym)
+        name = @@prompt.ask("Please enter your name:".green)
         user = User.find_by(username: username)
         if user 
-            message = "Sorry that username already exists"
+            message = "Sorry that username already exists.".green
         else
+            progress_bar
             User.create(username: username, password: password, name: name)
         end
 
     end
 
+    #PRODUCT METHODS AND HELPER METHODS#
+
+    def self.all_product_names
+        all_names = Product.all.map {|product| product.name}
+        all_names
+    end
+
+    def self.all_product_instances
+        Product.all
+    end
+    
+    def self.categories #Array of Pairing instances
+        Pairing.all
+    end
+
     def past_orders
         past_order = self.orders.where(checked_out: true)
-        if past_order.nil?
-        end 
     end
 
     def current_cart
@@ -44,7 +72,6 @@ class User < ActiveRecord::Base
     end
 
     def display_cart
-        
         self.current_cart.product_orders.each do |product_order|
             puts "Product Order ID: #{product_order.id}) #{product_order.product.name} Quantity: #{product_order.quantity}"
         end
@@ -54,15 +81,6 @@ class User < ActiveRecord::Base
         puts "Your total is: #{sprintf("$%2.2f",(@total_price.to_d/100))}"
     end
         
-    def self.all_product_names
-        all_names = Product.all.map {|product| product.name}
-        all_names
-    end
-
-    def self.all_product_instances
-        Product.all
-    end
-
     def update_quantity(product_order_id, new_quantity) #current_cart -> Order Instance
         product_order = self.current_cart.product_orders.find(product_order_id)
         product_order.update(quantity: new_quantity)
@@ -75,14 +93,14 @@ class User < ActiveRecord::Base
     def add_to_cart(product_instance, quantity)
         ProductOrder.create(order: self.current_cart, product: product_instance, quantity: quantity)
     end
-
-    def add_review
-        puts "Please write a review description, press Enter when done. (in a single line)"
-        review = gets.chomp
-        puts "Please add a rating (1-5)"
-        rating = gets.chomp.to_i
-        new_review = Review.create(rating: rating, description: review, product_id: $selection.id, user_id: self.id)
+    
+    def check_out_cart
+        self.display_cart
+        self.current_cart.update(checked_out: true)
+        puts "Thanks for the ez money 🤑"
     end
+
+    #REVIEW METHODS AND HELPER METHODS#
     
     def see_my_review_instances
         self.reviews
@@ -90,8 +108,22 @@ class User < ActiveRecord::Base
     
     def my_reviewed_products
         all_reviews = see_my_review_instances.map {|review| review.product}
-        if all_reviews.nil?
+    end
+
+    def delete_review_by_id(review_id)
+        self.reviews.find(review_id).destroy
+    end 
+
+    def add_review
+        puts "Please write a review description, press Enter when done. (in a single line)"
+        review = gets.chomp
+        puts "Please add a rating. (1-5)"
+        rating = gets.chomp.to_i
+        until (1..5).include?(rating)
+            puts "Please input a valid rating."
+            rating = gets.chomp.to_i
         end
+        new_review = Review.create(rating: rating, description: review, product_id: $selection.id, user_id: self.id)
     end
 
     def reviews_for_product
@@ -101,13 +133,4 @@ class User < ActiveRecord::Base
         end
     end
 
-    def self.categories #Array of Pairing instances
-        Pairing.all
-    end
-    
-    def check_out_cart
-        self.display_cart
-        self.current_cart.update(checked_out: true)
-        puts "Thanks for the ez money"
-    end
 end
